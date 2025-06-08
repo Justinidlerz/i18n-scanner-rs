c27185bc63f76f18e6090406cb7d64487c7ad5e4
Justinidlerz <zqc.sunny@gmail.com>
2025-06-08 10:51:23 +0800

    feat: update test

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from "vitest";
-import {scan} from '..'
+import {scan} from '../index'
 import * as path from 'node:path'
 
 const root = path.join(__dirname, './fake-project');
